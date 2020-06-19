3a1ce6709002544557cee80750d55a8f90690e50
Tophun <imtophun@gmail.com>
2020-06-19 13:44:34 +0900

    feat: first commit

---
+++ mergeplus/mergeplus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mergeplus
+//
+//  Created by Tophun_mh on 2020/06/19.
+//  Copyright © 2020 mergepoint. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
